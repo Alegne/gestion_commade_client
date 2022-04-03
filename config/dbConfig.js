8a9d056e0d2ece2b8b74a8a9c1f1f866f95fc2d9
LensAlegne <lensalegne@gmail.com>
2022-04-03 20:01:36 +0300

    fds

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 
-var mysql_host = process.env.MYSQL_HOST || 'localhost'
-var mysql_port = process.env.MYSQL_PORT || '3306'
-var mysql_DB = process.env.MYSQL_DB || 'gestion_commande'
-var mysql_user = process.env.MYSQL_USER || 'root'
-var mysql_password = process.env.MYSQL_PASSWORD || ''
+var mysql_host = process.env.DB_HOST || 'localhost'
+var mysql_port = process.env.DB_PORT || '3306'
+var mysql_DB = process.env.DB_NAME || 'gestion_commande'
+var mysql_user = process.env.DB_USER || 'root'
+var mysql_password = process.env.DB_PASSWORD || ''
 
 //creation mysql connexion
 const dbconn = mysql.createConnection({
