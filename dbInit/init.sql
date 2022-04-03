209f20533f8e3a03272bfc651168dfd90bd357ef
LensAlegne <lensalegne@gmail.com>
2022-04-03 17:08:24 +0300

    Ajout de Dockerfile et docker-compose

---
+++ dbInit/init.sql
@@ -0,0 +1,16 @@
+-- Listage de la structure de la base pour gestion_commande
+CREATE DATABASE IF NOT EXISTS `gestion_commande` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+USE `gestion_commande`;
+
+-- Listage de la structure de la table gestion_commande. client
+CREATE TABLE IF NOT EXISTS `client` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) COLLATE utf8_bin DEFAULT NULL,
+  `adresse` varchar(50) COLLATE utf8_bin DEFAULT NULL,
+  `cin` varchar(12) COLLATE utf8_bin DEFAULT NULL,
+  `phone` varchar(10) COLLATE utf8_bin DEFAULT NULL,
+  `create_at` date DEFAULT NULL,
+  `update_at` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQUE` (`cin`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')   //importation de body-parse
-
+const ip = require('ip')
+const cores = require('cores')
+const dotenv = require('dotenv')
 const clientRoutes = require('./src/routes/client.route')   //importation des client routes
 
-
+dotenv.config()
 
 //creation de l'express app
 const app = express()
 
 //definition du port d'ecoute
 const port = process.env.PORT || 3000
+
+//app.use(cores({origin: '*'}))
+app.use(express.json())
 
 //parse content data type urlencoded
 app.use(bodyParser.urlencoded({extend: false}))
@@ -23,4 +28,4 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.use('/api/v1/client', clientRoutes)
 
 //faire ecouter le server node js
-app.listen(port, () => console.log(`le serveur nodejs ecoute sur le port: ${port}!`))
+app.listen(port, () => console.log(`le serveur nodejs ecoute sur: ${ip.address()}:${port}!`))
